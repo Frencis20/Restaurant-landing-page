3a504a72e079c574837e2135787e0e5629215ea4
mazen <mazen.ali.rushdy@gmail.com>
2025-04-19 18:06:45 +0200

    feat: Structure, style, and animate the hero section

--- scripts.js
+++ scripts.js
@@ -14,3 +14,29 @@ if (hamburger && navLinks && overlay) {
     overlay.classList.remove("active");
   });
 }
+
+// Initialize ScrollReveal
+ScrollReveal({
+  reset: false,
+  distance: "60px",
+  duration: 800,
+  delay: 200,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  scale: 0.9,
+  mobile: false,
+});
+
+// Apply ScrollReveal animations to elements in the hero section
+ScrollReveal().reveal(".hero-content .hero-title", { origin: "top" });
+
+ScrollReveal().reveal(".hero-content .hero-subtitle", {
+  origin: "top",
+  delay: 300,
+});
+
+ScrollReveal().reveal(".hero-content .hero-button", {
+  origin: "bottom",
+  delay: 500,
+});
+
+ScrollReveal().reveal(".hero-image", { origin: "right", delay: 700 });
