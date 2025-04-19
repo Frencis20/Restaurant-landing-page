10bf69815ee8b38b5f498fe3e352e774bedf6cf5
mazen <mazen.ali.rushdy@gmail.com>
2025-04-19 18:25:02 +0200

    feat: Structure, style, and animate the chefs section

--- scripts.js
+++ scripts.js
@@ -84,7 +84,21 @@ ScrollReveal().reveal(".hero-content .hero-button", {
 });
 
 ScrollReveal().reveal(".hero-image", { origin: "right", delay: 700 });
+
 ScrollReveal().reveal(".specials-carousel .special-card", { interval: 200 });
+
 ScrollReveal().reveal(".explore-content", { origin: "left" });
+
 ScrollReveal().reveal(".explore-image", { origin: "right", delay: 300 });
+
 ScrollReveal().reveal(".services-grid .service-card", { interval: 200 });
+
+ScrollReveal().reveal(".chef-info", { origin: "left" });
+
+ScrollReveal().reveal(".chef-image", { origin: "right", delay: 300 });
+
+ScrollReveal().reveal(".chef-highlights li", {
+  interval: 200,
+
+  origin: "bottom",
+});
